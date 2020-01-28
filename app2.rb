d2a7f14774ef298f8c4db112f222e1d018c61b85
Cheltonne <chelton1506@gmail.com>
2020-01-28 08:32:15 +0100

    deuxieme commit

---
+++ app2.rb
@@ -0,0 +1,46 @@
+require 'bundler'
+Bundler.require
+
+# require_relative 'lib/game'
+require_relative 'lib/player'
+
+puts "------------------------------------------------
+|Bienvenue sur 'ILS VEULENT TOUS MA POO' !      |
+|Le but du jeu est d'être le dernier survivant !|
+-------------------------------------------------\n\n"
+
+puts "Comment t'appelles-tu ?"
+print "> "
+player1 = HumanPlayer.new(gets.chomp)
+ennemies = [cpu1 = Player.new("Goku"), cpu2 = Player.new("Saitama")]
+while (player1.life_points >= 1 && cpu1.life_points >= 1) || (player1.life_points >= 1 && cpu2.life_points >= 1)
+  puts "Il te reste #{player1.life_points} points de vie.\n\nQuelle action veux-tu effectuer ?\n"
+  puts "a - chercher une meilleure arme\ns - chercher à se soigner\n\n"
+  puts "attaquer un joueur en vue :\n0 - Goku a #{cpu1.life_points} points de vie\n1 - Saitama a #{cpu2.life_points} points de vie\n\n"
+  puts "Quel est ton choix ?"
+  print "> "
+  player_action = gets.chomp
+  if player_action == 'a'
+    player1.search_weapon
+  elsif player_action == 's'
+    player1.search_health_pack
+  elsif player_action == "0"
+    player1.attacks(cpu1)
+  elsif player_action == "1"
+    player1.attacks(cpu2)
+  else
+    puts "Ce choix n'existe pas."
+  end
+  if cpu1.life_points > 0 || cpu2.life_points > 0
+    puts "Les autres joueurs attaquent !"
+  end
+  ennemies.each do |ennemy|
+    ennemy.attacks(player1)
+    next if ennemy.life_points < 0
+  end
+end
+if player1.life_points >= 1
+  puts "Winner winner, chicken dinner"
+else
+  puts "Game over..."
+end
--- lib/player.rb
+++ lib/player.rb
@@ -18,7 +18,10 @@ attr_accessor :name, :life_points
   end
 
   def attacks(player)
-    puts "Le joueur #{name} attaque le joueur #{player.name}."
+    if life_points < 1
+      return true
+    end
+    puts "Le joueur #{@name} attaque le joueur #{player.name}."
     damage_dealt = compute_damage
     puts "Il lui inflige #{damage_dealt} de dommages."
     player.get_damage(damage_dealt)
@@ -28,4 +31,57 @@ attr_accessor :name, :life_points
     return rand(1..6)
   end
 
-end #Fin de la definition de la classe player
+end #Fin de la definition de la classe Player
+
+class HumanPlayer < Player
+attr_accessor :weapon_level
+
+def initialize(name)
+  @life_points = 100
+  @weapon_level = 1
+end
+
+def show_state
+  puts "#{name} a #{life_points} points de vie, et une arme de niveau #{weapon_level}."
+end
+
+def compute_damage
+  rand(1..6) * @weapon_level
+end
+
+def search_weapon
+  new_weapon_level = rand(1..6)
+  puts "Tu as trouve une arme de niveau #{new_weapon_level}"
+  if new_weapon_level > @weapon_level
+    @weapon_level = new_weapon_level
+    puts "Oh que oui, elle est meilleure que ton arme actuelle, tu la prends donc."
+  else
+    puts "Oh que non...elle n'est pas mieux que ton arme actuelle..."
+  end
+end
+
+def search_health_pack
+  dice_result = rand(1..6)
+  if dice_result == 1
+    puts "Tu n'as rien trouve..."
+  elsif dice_result >= 2 || dice_result <= 5
+    puts "Bravo, tu as trouvé un pack de +50 points de vie !"
+    if @life_points >= 51
+      until @life_points == 100
+        @life_points += 1
+      end
+    else
+      @life_points += 50
+    end
+  else
+    puts "OH QUE OUI, tu as trouvé un pack de +80 points de vie !"
+    if @life_points >= 21
+      until @life_points == 100
+        @life_points += 1
+      end
+    else
+      @life_points += 80
+    end
+  end
+end
+end #Fin de la definition de classe HumanPlayer
